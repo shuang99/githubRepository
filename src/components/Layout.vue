38e6a24970b9d340609baf2aa27db08970fcb3ea
liuguiting <liuguiting@hydsoft.com>
2021-11-25 18:04:58 +0800

    菜单展开折叠

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -2,20 +2,38 @@
   <el-container>
     <el-header class="topContent"> <top-nav /></el-header>
     <el-container>
-      <el-aside><side-menu /></el-aside>
+      <el-aside :class="store.isCollapse ? 'sideWidth' : ''"
+        ><side-menu
+      /></el-aside>
       <el-main><main-content /></el-main>
     </el-container>
   </el-container>
 </template>
 
 <script lang='ts'>
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import MainContent from "./MainContent.vue";
 import SideMenu from "./SideMenu.vue";
 import TopNav from "./TopNav.vue";
+import { appStore } from "../store/appStore";
+
+interface State {
+  collapseWidth: string;
+}
 
 export default defineComponent({
   components: { SideMenu, TopNav, MainContent },
+  setup() {
+    const collapseWidth = ref<State>({
+      collapseWidth: "240px",
+    });
+    const store = appStore();
+
+    return {
+      collapseWidth,
+      store,
+    };
+  },
 });
 </script>
 <style lang='scss' scoped>
@@ -25,5 +43,9 @@ export default defineComponent({
   margin: 0;
   display: flex;
   align-items: center;
+}
+.sideWidth {
+  width: 80px;
+  overflow-x: hidden;
 }
 </style>
--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -3,8 +3,6 @@
     default-active="2"
     class="el-menu-vertical-demo"
     :collapse="store.isCollapse"
-    @open="handleOpen"
-    @close="handleClose"
   >
     <base-sub-menu :routes="routesList"></base-sub-menu>
   </el-menu>
@@ -12,40 +10,21 @@
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
-import {
-  Location,
-  Document,
-  Menu as IconMenu,
-  Setting,
-} from "@element-plus/icons";
 import { routes } from "../router";
 import BaseSubMenu from "./menu/BaseSubMenu.vue";
-import { appStore  } from "../store/appStore";
+import { appStore } from "../store/appStore";
 
 export default defineComponent({
   components: {
-    Location,
-    Document,
-    Setting,
-    IconMenu,
     BaseSubMenu,
   },
   setup() {
-    const routesList =ref(routes)
-     const store = appStore();
-    
-    const handleOpen = (key: string, keyPath: string) => {
-      console.log(key, keyPath);
-    };
-    const handleClose = (key: string, keyPath: string) => {
-      console.log(key, keyPath);
-    };
-    
+    const routesList = ref(routes);
+    const store = appStore();
+
     return {
-      handleOpen,
-      handleClose,
       routesList,
-      store
+      store,
     };
   },
 });
